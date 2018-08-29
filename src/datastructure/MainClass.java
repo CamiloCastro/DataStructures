cb237154ddf8f5af91f8f2fb27a075d2f730a626
SC\juan.castro <juan.castro@software-colombia.com>
2018-08-29 15:45:39 -0500

    NumArrayList.

--- src/datastructure/MainClass.java
+++ src/datastructure/MainClass.java
@@ -15,8 +15,18 @@ public class MainClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println(1 >> 1);
+        
+        NumArrayList list = new NumArrayList();
+        
+        list.add(15);
+        list.add(9);
+        list.add(-3);
+        list.add(5);
+        System.out.println("Size: " + list.size());
+        list.remove(2);
+        System.out.println("Size: " + list.size());
+        list.print();
+        
     }
     
 }
--- src/datastructure/NumArrayList.java
+++ src/datastructure/NumArrayList.java
@@ -1,3 +1,5 @@
+package datastructure;
+
 public class NumArrayList {
 
     private int size;
@@ -6,19 +8,102 @@ public class NumArrayList {
     public NumArrayList(int initialCapacity) {
         if (initialCapacity > 0) {
             this.elementData = new Integer[initialCapacity];
-        } else if (initialCapacity == 0) {
+        } else
+        {
             this.elementData = new Integer[0];
-        } else {
-            throw new IllegalArgumentException("Illegal Capacity: "+
-                    initialCapacity);
         }
+        this.size = 0;
     }
 
     public NumArrayList() {
         this.elementData = new Integer[0];
+        this.size = 0;
     }
 
+    private void ensureCapacity(int minCapacity)
+    {
+        if (elementData.length < minCapacity)
+        {
+            int newArraySize;
+            if (elementData.length == 0)
+                newArraySize = 1;
+            else
+                newArraySize = elementData.length * 2;
+            
+            Integer[] newArray = new Integer[newArraySize];
+            for (int i = 0; i < elementData.length; i++) {
+                newArray[i] = elementData[i];                
+            }
+            elementData = newArray;            
+        }
+    }
 
+    public void add(Integer val)
+    {
+        ensureCapacity(size + 1);
+        elementData[size++] = val;
+    }
+    
+    public void add(int index, Integer val)
+    {
+        if(index >= 0 && index <= size)
+        {
+            ensureCapacity(size + 1);
+            for (int i = index; i < elementData.length - 1; i++) {
+                elementData[i + 1] = elementData[i];                
+            }
+            elementData[index] = val;
+            size++;
+        }
+    }
+    
+    public Integer get(int index)
+    {
+        if (index > 0 && index < size)
+            return elementData[index];
+        else
+            return null;
+    }
+    
+    public void set(int index, Integer value)
+    {
+        if (index > 0 && index < size)
+        {
+            elementData[index] = value;
+        }
+    }
+    
+    public int size()
+    {
+        return size;
+    }
+    
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+    
+    public void remove(int index)
+    {
+        if (index > 0 && index < size)
+        {
+            for (int i = index; i < elementData.length - 1; i++) {
+                elementData[i] = elementData[i + 1];                
+            }
+            elementData[size - 1] = null;
+            size--;
+        }
+    }
+    
+    public void print()
+    {
+        for (int i = 0; i < size; i++) {
+            System.out.print(elementData[i] + " ");            
+        }
+        System.out.println("");              
+    }
+    
+    
 
 
 
