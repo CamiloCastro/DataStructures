c349e4898a3d934253e6ae55d1fac3e2834dc8e4
Juan Camilo Castro Pinto <jccastrop2012@gmail.com>
2018-08-29 23:03:44 -0500

    Interface of list

---
+++ src/datastructure/AbstractList.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructure;
+
+/**
+ *
+ * @author jccas
+ */
+public abstract class AbstractList<T> implements List<T> {
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T get(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(T element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(int index, T element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void set(int index, T element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void print() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void sort() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
---
+++ src/datastructure/ArrayList.java
@@ -0,0 +1,119 @@
+package datastructure;
+
+public class ArrayList {
+
+    private int size;
+    private Integer[] elementData;
+
+    public ArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Integer[initialCapacity];
+        } else
+        {
+            this.elementData = new Integer[0];
+        }
+        this.size = 0;
+    }
+
+    public ArrayList() {
+        this.elementData = new Integer[0];
+        this.size = 0;
+    }
+
+    private void ensureCapacity(int minCapacity)
+    {
+        if (elementData.length < minCapacity)
+        {
+            int newArraySize;
+            if (elementData.length == 0)
+                newArraySize = 1;
+            else
+                newArraySize = elementData.length * 2;
+            
+            Integer[] newArray = new Integer[newArraySize];
+            for (int i = 0; i < elementData.length; i++) {
+                newArray[i] = elementData[i];                
+            }
+            elementData = newArray;            
+        }
+    }
+
+    public void add(Integer val)
+    {
+        ensureCapacity(size + 1);
+        elementData[size++] = val;
+    }
+    
+    public void add(int index, Integer val)
+    {
+        if(index >= 0 && index <= size)
+        {
+            ensureCapacity(size + 1);
+            for (int i = index; i < elementData.length - 1; i++) {
+                elementData[i + 1] = elementData[i];                
+            }
+            elementData[index] = val;
+            size++;
+        }
+    }
+    
+    public Integer get(int index)
+    {
+        if (index > 0 && index < size)
+            return elementData[index];
+        else
+            return null;
+    }
+    
+    public void set(int index, Integer value)
+    {
+        if (index > 0 && index < size)
+        {
+            elementData[index] = value;
+        }
+    }
+    
+    public int size()
+    {
+        return size;
+    }
+    
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+    
+    public void remove(int index)
+    {
+        if (index > 0 && index < size)
+        {
+            for (int i = index; i < elementData.length - 1; i++) {
+                elementData[i] = elementData[i + 1];                
+            }
+            elementData[size - 1] = null;
+            size--;
+        }
+    }
+    
+    public void print()
+    {
+        for (int i = 0; i < size; i++) {
+            System.out.print(elementData[i] + " ");            
+        }
+        System.out.println("");              
+    }
+    
+    public void clear()
+    {
+        for (int i = 0; i < size; i++)
+            elementData[i] = null;
+
+        size = 0;
+    }
+    
+    
+
+
+
+
+}
